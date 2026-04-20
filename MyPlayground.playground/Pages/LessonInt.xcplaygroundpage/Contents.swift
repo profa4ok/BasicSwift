45ca241a8d62d96fcf55fb9d66889d3476cb373b
profa4oka <profa4oka98@gmail.com>
2026-04-20 18:31:34 +0300

    Lesson Int

--- MyPlayground.playground/Pages/LessonInt.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/LessonInt.xcplaygroundpage/Contents.swift
@@ -2,6 +2,31 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+var greeting = "Lesson Int"
 
-//: [Next](@next)
+let valueInt = 5
+
+print("Int.min = \(Int.min)")
+print("Int.max = \(Int.max)")
+
+print("Int8.min = \(Int8.min)")
+print("Int8.max = \(Int8.max)")
+
+print("Int16.min = \(Int16.min)")
+print("Int16.max = \(Int16.max)")
+
+print("Int32.min = \(Int32.min)")
+print("Int32.max = \(Int32.max)")
+
+print("Int64.min = \(Int64.min)")
+print("Int64.max = \(Int64.max)")
+
+print("Int128.min = \(Int128.min)")
+print("Int128.max = \(Int128.max)")
+
+print("UInt.max = \(UInt.max)")
+print("UInt8.max = \(UInt8.max)")
+print("UInt16.max = \(UInt16.max)")
+print("UInt32.max = \(UInt32.max)")
+print("UInt64.max = \(UInt64.max)")
+print("UInt128.max = \(UInt128.max)")
